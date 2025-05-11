712707001a23ce3641b5019bb32f0ff9a878106b
orlifera <oferazzani125@gmail.com>
2025-05-11 21:34:44 +0200

    special char fix

--- src/components/ui/loader.tsx
+++ src/components/ui/loader.tsx
@@ -6,7 +6,7 @@ function loader() {
         <div className="w-full h-screen m-auto flex flex-col items-center justify-center">
             <div className="w-10 h-10 border-4 border-t-primary border-gray-300 rounded-full animate-spin"></div>
             <h3 className="text-2xl text-gray-500 font-bold mb-4">Attendi un secondo</h3>
-            <p className='text-gray-500 font-semibold'>Stiamo caricando il contenuto. Porta un po' di pazienza <span role="decoration">☺️</span></p>
+            <p className='text-gray-500 font-semibold'>Stiamo caricando il contenuto. Porta un po&apos; di pazienza <span role="decoration">☺️</span></p>
         </div>
     )
 }
