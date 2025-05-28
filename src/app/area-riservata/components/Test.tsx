6a579247da432e6bbb0bc0331dff5d169caf5877
orlifera <oferazzani125@gmail.com>
2025-05-28 10:02:45 +0200

    hotfix

--- src/app/area-riservata/components/Test.tsx
+++ src/app/area-riservata/components/Test.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Test() {
     return (
         <div className='w-full h-full flex flex-col items-center justify-center'>
-            qua andrà la parte dove controllare l&apos;attività di laboratorio come video e l&apos;avanzamento degli step.
+            qua andrà la parte dove potrà visualizzare i video.
         </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -204,7 +204,7 @@ export default function Home() {
           </div>
           <div id="">
             <h2 className="text-2xl font-bold text-white mt-4 text-center">Inizia subito!</h2>
-            <Link id="inizia" href="/lab" className="flex p-3 m-4 rounded bg-gradient-to-tl bg-primary hover:bg-gradient-to-br hover:from-white/50 hover:bg-white hover:text-primary hover:translate-1 hover:shadow-lg text-white items-center justify-center visited:text-white">
+            <Link id="inizia" href="/lab" className="flex p-3 m-4 rounded bg-gradient-to-tl bg-primary hover:bg-gradient-to-br hover:from-white/50 hover:bg-white hover:text-primary hover:translate-1 hover:shadow-lg  items-center justify-center visited:hover:text-primary">
               <LinkIcon className="m-1" /> Vai al laboratorio
             </Link>
           </div>
